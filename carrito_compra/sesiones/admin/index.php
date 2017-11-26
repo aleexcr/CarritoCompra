502bbf3f8acfb0ce592c796104e7d506f29a0402
Alejandro <34007680+aleexcr@users.noreply.github.com>
2017-11-26 12:07:17 -0600

    Add files via upload

---
+++ carrito_compra/sesiones/admin/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<LINK REL=StyleSheet HREF="../../bootstrap/css/bootstrap.css" TYPE="text/css" MEDIA=screen>
+<LINK REL=StyleSheet HREF="../../bootstrap/css/bootstrap.min.css" TYPE="text/css" MEDIA=screen>
+<script type="text/javascript" src="../../bootstrap/js/bootstrap.js"></script>
+<script type="text/javascript" src="../../bootstrap/js/bootstrap.js"></script>
+
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<title>Carrito de compras</title>
+</head>
+<body>
+<div class="row">
+	<div class="col-md-12">
+		<nav class="navbar navbar-inverse navbar-fixed-top" style="background-color: #0A0A2A">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#"><img src="../../image/carrito.png" width="40px"></a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Inicio</a></li>
+      <li><a href="#">Proceso cursos</a></li>
+      <li><a href="#">Proceso usuarios</a></li>
+      <li><a href="#">Configuracion</a></li>
+    </ul>
+  </div>
+</nav>
+	</div>
+</div>
+
+		<div class="row">
+  <div class="col">col</div>
+  <div class="col">col</div>
+  <div class="col">col</div>
+  <div class="col">col</div>
+</div>
+<div class="row">
+  <div class="col-8">col-8</div>
+  <div class="col-4">col-4</div>
+</div>
+	
+
+
+
+</body>
+</html>
